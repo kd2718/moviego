8b93daefebc9cc7475c7487e9d13e1c1e1aa2eb3
kory donati <kd2718@outlook.com>
2017-10-24 23:57:21 -0700

    added host id

--- app.go
+++ app.go
@@ -3,10 +3,13 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    var host, _ = os.Hostname()
     fmt.Fprintf(w, "Hi there, I path is %s!", r.URL.Path[1:])
+    fmt.Fprint(w, "Host Name: %s!!", host)
 }
 
 func main() {
