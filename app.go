b471f9a2116ee8521550a77203112dfbf94d1058
kory donati <kd2718@outlook.com>
2017-10-27 00:16:46 -0700

    redis running

--- app.go
+++ app.go
@@ -7,18 +7,14 @@ import (
     "github.com/go-redis/redis"
 )
 
-//var client = *redis.Client;
+var client *redis.Client
 
 func handler(w http.ResponseWriter, r *http.Request) {
     /*client = redis.NewClient(&redis.Options{
         Addr: "redis:6379"
     })
     */
-    client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+    
     result, err := client.Incr("counter").Result();
     w.Header().Set("Content-Type", "text/html; charset=utf-8")
     var host, _ = os.Hostname()
@@ -28,7 +24,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+    client := redis.NewClient(&redis.Options{
+        Addr:     "redis:6379",
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+
     fmt.Println("Starting service")
+    pong, err := client.Ping().Result();
+    fmt.Println("pong: ", pong, " err: ", err)
     http.HandleFunc("/", handler)
     http.ListenAndServe(":80", nil)
 }
