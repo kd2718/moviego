8bf8a68391967403e549d4e86bc0e86eab6eddd2
kory donati <kd2718@outlook.com>
2017-10-27 00:12:15 -0700

    stuff kinda working...

--- app.go
+++ app.go
@@ -4,16 +4,31 @@ import (
     "fmt"
     "net/http"
     "os"
+    "github.com/go-redis/redis"
 )
 
+//var client = *redis.Client;
+
 func handler(w http.ResponseWriter, r *http.Request) {
+    /*client = redis.NewClient(&redis.Options{
+        Addr: "redis:6379"
+    })
+    */
+    client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+    result, err := client.Incr("counter").Result();
     w.Header().Set("Content-Type", "text/html; charset=utf-8")
     var host, _ = os.Hostname()
     fmt.Fprintf(w, "Hi there, I path is %s!", r.URL.Path[1:])
     fmt.Fprintf(w, "\n<h3>Host Name: %s!!</h3>", host)
+    fmt.Fprintf(w, "count: ", result, " err: ", err)
 }
 
 func main() {
+    fmt.Println("Starting service")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":80", nil)
 }
